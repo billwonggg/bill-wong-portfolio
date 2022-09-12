56e64547ca6aecc5e2e43e185ffde2356796541a
Bill Wong <billwong7788@gmail.com>
2022-09-12 22:17:02 +1000

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
+import AboutPage from "./pages/About/AboutPage";
+import HomePage from "./pages/Home/HomePage";
+import ProjectPage from "./pages/Projects/ProjectPage";
+import SkillsPage from "./pages/Skills/SkillsPage";
+import { AnimatePresence } from "framer-motion";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <AnimatePresence initial={false}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/projects" element={<ProjectPage />} />
+          <Route path="/skills" element={<SkillsPage />} />
+        </Routes>
+      </AnimatePresence>
     </BrowserRouter>
   );
 };
