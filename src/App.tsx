0c9a34140e57f8619a48f8e0297292312ebe5e53
Bill Wong <billwong7788@gmail.com>
2022-09-12 21:53:03 +1000

    browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
