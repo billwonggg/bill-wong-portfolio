c514db96d25ce734075c7e52341ada7fc2a6c436
Bill Wong <billwong7788@gmail.com>
2022-09-12 16:35:53 +1000

    portfolio model

---
+++ src/models/Portfolio.tsx
@@ -0,0 +1,16 @@
+import { Education } from "./Education";
+import { Project } from "./Project";
+import { Technology } from "./Technology";
+import { Experience } from "./Experience";
+
+export interface Portfolio {
+  homeProfile: {
+    headlines: string[];
+    description: string;
+  };
+
+  projects: Project[];
+  work: Experience[];
+  education: Education;
+  skills: Technology[];
+}
