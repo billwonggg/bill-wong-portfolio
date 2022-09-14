2384cc6cae9ceb5e3e199fe555928d72f1c65303
Bill Wong <billwong7788@gmail.com>
2022-09-14 17:02:56 +1000

    add experience

---
+++ content/MyExperience.tsx
@@ -0,0 +1,20 @@
+import Experience from "../src/models/Experience";
+
+const MyExperience: Experience[] = [
+  {
+    title: "AWS Early Careers Innovation Program",
+    company: "Amazon",
+    from: "May 2022",
+    to: "June 2022",
+    technologies: ["AWS"],
+  },
+  {
+    title: "Accenture Tech Bootcamp",
+    company: "Accenture",
+    from: "Feb 2022",
+    to: "Feb 2022",
+    technologies: ["React", "MaterialUI", "AWS"],
+  },
+];
+
+export default MyExperience;
