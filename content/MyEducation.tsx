35c57a99d6f26ca2b7f173cfff3c535a0770e27d
Bill Wong <billwong7788@gmail.com>
2022-09-14 17:02:41 +1000

    add education

---
+++ content/MyEducation.tsx
@@ -0,0 +1,10 @@
+import Education from "../src/models/Education";
+
+const MyEducation: Education = {
+  degree: "Bachelor of Engineering (Honours), Software Engineering",
+  university: "UNSW",
+  from: "Feb 2021",
+  to: "Nov 2024",
+};
+
+export default MyEducation;
