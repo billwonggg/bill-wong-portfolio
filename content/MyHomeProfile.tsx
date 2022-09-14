28db8b0b0a3950663a22a276f0ef79324a65a9d4
Bill Wong <billwong7788@gmail.com>
2022-09-14 17:03:34 +1000

    add home profile

---
+++ content/MyHomeProfile.tsx
@@ -0,0 +1,8 @@
+import HomeProfile from "../src/models/HomeProfile";
+
+const MyHomeProfile: HomeProfile = {
+  headlines: ["second year student."],
+  description: "Second Year Software Engineering Student at UNSW",
+};
+
+export default MyHomeProfile;
