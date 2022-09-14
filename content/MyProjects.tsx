5f0554a150559869408f46b45de8740a8eb62c57
Bill Wong <billwong7788@gmail.com>
2022-09-14 17:03:07 +1000

    add projects

---
+++ content/MyProjects.tsx
@@ -0,0 +1,17 @@
+import Project from "../src/models/Project";
+
+const MyProjects: Project[] = [
+  {
+    title: "MessageSpace",
+    tools: ["React", "MaterialUI", "Azure", "SocketIO", "Docker"],
+    description:
+      "Built the frontend with React.js and Material UI and the backend with Express.js and \
+      Socket.io for bi-directional client-server communication, allowing users to join rooms \
+      and send messages in real time.",
+    imageUrl: "",
+    demoUrl: "https://message-space.web.app",
+    gitHubUrl: "https://github.com/billwonggg/message-space",
+  },
+];
+
+export default MyProjects;
