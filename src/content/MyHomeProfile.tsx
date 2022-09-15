9a9e8c41a50b72919745a4f1950aad385a22138a
Bill Wong <billwong7788@gmail.com>
2022-09-15 21:34:20 +1000

    update headlines

--- src/content/MyHomeProfile.tsx
+++ src/content/MyHomeProfile.tsx
@@ -3,8 +3,10 @@ import HomeProfile from "../models/HomeProfile";
 const MyHomeProfile: HomeProfile = {
   headlines: [
     "a second year Software Engineering student.",
-    "an aspiring Software Engineer.",
     "passionate about technology and innovation.",
+    "an aspiring Software Engineer.",
+    "eager to learn new skills.",
+    "a hobbyist competitive programmer.",
   ],
   description: "Second Year Software Engineering Student at UNSW",
 };
