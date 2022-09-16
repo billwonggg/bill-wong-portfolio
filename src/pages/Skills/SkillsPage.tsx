fe00ff61a556c3c1859c557cfad21f9c1f1c1946
Bill Wong <billwong7788@gmail.com>
2022-09-16 13:27:56 +1000

    skills page

--- src/pages/Skills/SkillsPage.tsx
+++ src/pages/Skills/SkillsPage.tsx
@@ -1,14 +1,19 @@
 import { motion } from "framer-motion";
+import SkillCloud from "../../components/Skills/SkillCloud";
+import styles from "./SkillsPage.module.scss";
 
 const SkillsPage = () => {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1, delay: 0.5 }}
-    >
-      My most used programming languages and frameworks are:
-    </motion.div>
+    <div className={styles.skillspage}>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 0.5 }}
+      >
+        <h1>Skills</h1>
+        <SkillCloud />
+      </motion.div>
+    </div>
   );
 };
 
