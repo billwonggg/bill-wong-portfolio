2610483b53b22007246cc949ee9af01c559fb7d9
Bill Wong <billwong7788@gmail.com>
2022-09-16 13:27:18 +1000

    update skills content

--- src/content/MySkills.tsx
+++ src/content/MySkills.tsx
@@ -1,5 +1,30 @@
-import { Technology } from "../models/Technology";
+import { siJavascript, siC, siCplusplus, siReact, siPython } from "simple-icons/icons";
+import { siTypescript, siNodedotjs, siAmazonaws, siMicrosoftazure } from "simple-icons/icons";
+import { siDocker, siGooglecloud, siHtml5, siCss3, siGnubash } from "simple-icons/icons";
+import { siFirebase, siGit, siVisualstudiocode, siPostgresql } from "simple-icons/icons";
+import { siExpress, siGithub } from "simple-icons/icons";
 
-const MySkills: Technology[] = ["C", "C++", "Java", "Python"];
+const MySkills = [
+  siC,
+  siCplusplus,
+  siPython,
+  siHtml5,
+  siCss3,
+  siJavascript,
+  siTypescript,
+  siReact,
+  siNodedotjs,
+  siGnubash,
+  siDocker,
+  siAmazonaws,
+  siMicrosoftazure,
+  siGooglecloud,
+  siFirebase,
+  siGit,
+  siVisualstudiocode,
+  siPostgresql,
+  siExpress,
+  siGithub,
+];
 
 export default MySkills;
