61c37c271d07d5c943917d82279ac4cda871977f
Bill Wong <billwong7788@gmail.com>
2022-09-17 17:49:30 +1000

    initialise projects page style

--- src/pages/Projects/ProjectPage.tsx
+++ src/pages/Projects/ProjectPage.tsx
@@ -1,14 +1,23 @@
 import { motion } from "framer-motion";
+import HomeLink from "../../components/Links/HomeLink";
+import SkillsLink from "../../components/Links/SkillsLink";
+import styles from "./ProjectPage.module.scss";
 
 const ProjectPage = () => {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1, delay: 0.5 }}
-    >
-      MessageSpace
-    </motion.div>
+    <div className={styles.projectspage}>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1, delay: 0.5 }}
+        className={styles.projectsInner}
+      >
+        <HomeLink {...{ position: "absolute", top: "20px", left: 0 }} />
+        <SkillsLink {...{ position: "absolute", top: "20px", right: 0 }} />
+        Hello
+      </motion.div>
+    </div>
   );
 };
 
