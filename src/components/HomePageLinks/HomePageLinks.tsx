09e7c9d8ba1b0b564698873bfd91108772e0f948
Bill Wong <billwong7788@gmail.com>
2022-09-19 17:27:57 +1000

    responsive display for links

--- src/components/HomePageLinks/HomePageLinks.tsx
+++ src/components/HomePageLinks/HomePageLinks.tsx
@@ -1,27 +1,12 @@
 import React from "react";
-import styles from "./HomePageLinks.module.scss";
-import AboutLink from "../Links/AboutLink";
-import ProjectsLink from "../Links/ProjectsLink";
-import SkillsLink from "../Links/SkillsLink";
-import ExperienceLink from "../Links/ExperienceLink";
+import { useWidthBreakpoint } from "../../util/breakpoint";
+import HomePageLinksLg from "./HomePageLinksLg";
+import HomePageLinksMd from "./HomePageLinksMd";
 
 const HomePageLinks = () => {
-  const linkStyles: React.CSSProperties = {
-    position: "relative",
-    bottom: "6px",
-    margin: "0px 50px",
-  };
+  const md = useWidthBreakpoint(900);
 
-  return (
-    <div className={styles.linkcontainer}>
-      <div className={styles.linksInner}>
-        <AboutLink {...linkStyles} />
-        <ProjectsLink {...linkStyles} />
-        <ExperienceLink {...linkStyles} />
-        <SkillsLink {...linkStyles} />
-      </div>
-    </div>
-  );
+  return md ? <HomePageLinksMd /> : <HomePageLinksLg />;
 };
 
 export default HomePageLinks;
