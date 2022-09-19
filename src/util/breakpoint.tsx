2cf6883b20744be7b7c0e43ac142a13fad6c93ae
Bill Wong <billwong7788@gmail.com>
2022-09-19 17:28:44 +1000

    add screen breakpoint hook

---
+++ src/util/breakpoint.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+/**
+ * Hook that returns whether the current window width is less than the breakpoint width.
+ */
+export const useWidthBreakpoint = (breakpoint: number): boolean => {
+  const [windowWidth, setWindowWidth] = useState<number>(0);
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [windowWidth]);
+
+  return windowWidth <= breakpoint;
+};
