71baee94a3091df76c89c01aad7598e54d59ec6d
Bill Wong <billwong7788@gmail.com>
2022-09-19 21:00:59 +1000

    update experience link icon

--- src/components/Links/ExperienceLink.tsx
+++ src/components/Links/ExperienceLink.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Link.module.scss";
-import { IoBriefcaseOutline } from "react-icons/io5";
+import { HiOutlineBriefcase } from "react-icons/hi";
 
 const ExperienceLink = (style: React.CSSProperties) => {
   return (
     <Link className={styles.link} to="/experience" style={style}>
-      <IoBriefcaseOutline size="0.95em" style={{ position: "relative", top: "2px" }} />
+      <HiOutlineBriefcase size="0.95em" style={{ position: "relative", top: "4px" }} />
       &nbsp;Experience
     </Link>
   );
