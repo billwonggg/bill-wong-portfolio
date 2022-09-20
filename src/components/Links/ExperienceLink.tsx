1def368071d2eab03b03e6f40c310d6f776a3cd3
Bill Wong <billwong7788@gmail.com>
2022-09-20 10:43:53 +1000

    icon positioning

--- src/components/Links/ExperienceLink.tsx
+++ src/components/Links/ExperienceLink.tsx
@@ -6,7 +6,7 @@ import { HiOutlineBriefcase } from "react-icons/hi";
 const ExperienceLink = (style: React.CSSProperties) => {
   return (
     <Link className={styles.link} to="/experience" style={style}>
-      <HiOutlineBriefcase size="0.95em" style={{ position: "relative", top: "4px" }} />
+      <HiOutlineBriefcase size="0.95em" style={{ position: "relative", top: "3px" }} />
       &nbsp;Experience
     </Link>
   );
