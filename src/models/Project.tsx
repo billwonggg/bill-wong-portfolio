295d3b72faab3b50c9573340491a8c54d215a5ed
Bill Wong <billwong7788@gmail.com>
2022-09-23 10:43:10 +1000

    update project model

--- src/models/Project.tsx
+++ src/models/Project.tsx
@@ -1,11 +1,12 @@
+import { JsxElement } from "typescript";
 import { Technology } from "./Technology";
 
 export default interface Project {
   title: string;
   headline?: string;
-  description: string;
+  description?: string;
   tools: Technology[];
-  imageUrl: string;
+  image?: JSX.Element;
   gitHubUrl?: string;
   demoUrl?: string;
 }
