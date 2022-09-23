9624f125158c934d8edb4d17176036d679164c34
Bill Wong <billwong7788@gmail.com>
2022-09-23 23:08:11 +1000

    create experience card

--- src/components/ExperienceCard/ExperienceCard.tsx
+++ src/components/ExperienceCard/ExperienceCard.tsx
@@ -1,8 +1,24 @@
 import Experience from "../../models/Experience";
+import GitHubButton from "../Buttons/GitHubButton";
+import WebsiteButton from "../Buttons/WebsiteButton";
 import styles from "./ExperienceCard.module.scss";
 
 const ExperienceCard = (props: Experience) => {
-  return <div className={styles.projectCard}></div>;
+  return (
+    <div className={styles.experienceCard}>
+      <h3>{props.title}</h3>
+      <div className={styles.logoLine}>
+        <p>{props.timeline}</p>
+        <div className={styles.imgContainer}>{props.company}</div>
+      </div>
+      {props.image !== undefined && <div className={styles.demoImage}>{props.image}</div>}
+      {props.description}
+      <div className={styles.experienceLinks}>
+        {props.gitHubUrl && <GitHubButton url={props.gitHubUrl} />}
+        {props.demoUrl && <WebsiteButton url={props.demoUrl} />}
+      </div>
+    </div>
+  );
 };
 
 export default ExperienceCard;
