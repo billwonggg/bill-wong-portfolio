3cff81ce9efa3541dc71b4dda9acdcc2be50133b
Bill Wong <billwong7788@gmail.com>
2022-09-23 11:32:39 +1000

    progress on experience

---
+++ src/components/ExperienceCard/ExperienceCard.tsx
@@ -0,0 +1,8 @@
+import Experience from "../../models/Experience";
+import styles from "./ExperienceCard.module.scss";
+
+const ExperienceCard = (props: Experience) => {
+  return <div className={styles.projectCard}></div>;
+};
+
+export default ExperienceCard;
--- src/models/Experience.tsx
+++ src/models/Experience.tsx
@@ -2,7 +2,7 @@ import { Technology } from "./Technology";
 
 export default interface Experience {
   title: string;
-  company: string;
+  company?: string;
   location?: string;
   employmentType?: "Full-Time" | "Part-Time" | "Internship" | "Casual" | "Program";
   team?: string;
