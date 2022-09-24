6a037dae72c3df8f1c2ee6227fd1315dc276cba8
Bill Wong <billwong7788@gmail.com>
2022-09-24 15:52:21 +1000

    add certification data

---
+++ src/content/MyCertifications.tsx
@@ -0,0 +1,17 @@
+import Certification from "../models/Certification";
+
+const MyCertifications: Certification[] = [
+  {
+    name: "Azure Fundamentals Certification",
+    url: "https://www.credly.com/badges/716a3c46-cd1b-4788-bb53-e15c3a0a8605",
+  },
+  {
+    name: "Google Cloud Certification",
+    url: "https://www.cloudskillsboost.google/public_profiles/3619bacc-273e-4485-82e6-9e190832607a",
+  },
+  {
+    name: "Microsoft Excel Expert Certification",
+    url: "https://www.credly.com/badges/aeb27d10-5b44-479b-a3dd-a9a0a92bebcd?source=linked_in_profile",
+  },
+];
+export default MyCertifications;
