aa2b8a4a82ba31599c9a80b62733a78c62f0c864
Bill Wong <billwong7788@gmail.com>
2022-09-27 00:08:12 +1000

    change technology model to include icons

--- src/models/Technology.tsx
+++ src/models/Technology.tsx
@@ -1,14 +1,24 @@
-export type Technology =
-  | "AWS"
-  | "Azure"
-  | "Docker"
-  | "Express"
-  | "GCP"
-  | "JavaScript"
-  | "Jest"
-  | "MaterialUI"
-  | "Node"
-  | "React"
-  | "SCSS"
-  | "Socket.io"
-  | "TypeScript";
+import {
+  SiAmazonaws,
+  SiDocker,
+  SiExpress,
+  SiJest,
+  SiMaterialui,
+  SiMicrosoftazure,
+  SiReact,
+  SiSocketdotio,
+  SiTypescript,
+} from "react-icons/si";
+import { Badge } from "../components/ToolBadges/ToolBadges";
+
+export const Technology: { [key: string]: Badge } = {
+  AWS: { name: "AWS", icon: SiAmazonaws },
+  Azure: { name: "Azure", icon: SiMicrosoftazure },
+  Docker: { name: "Docker", icon: SiDocker },
+  Express: { name: "Express", icon: SiExpress },
+  Jest: { name: "Jest", icon: SiJest },
+  MaterialUI: { name: "MaterialUI", icon: SiMaterialui },
+  React: { name: "React", icon: SiReact },
+  SocketIO: { name: "Socket.io", icon: SiSocketdotio },
+  TypeScript: { name: "TypeScript", icon: SiTypescript },
+};
