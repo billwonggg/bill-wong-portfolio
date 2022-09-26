ef5b238e288f378cb9bd2c1e7bd599720048c26b
Bill Wong <billwong7788@gmail.com>
2022-09-27 00:24:03 +1000

    add color to badges

--- src/models/Technology.tsx
+++ src/models/Technology.tsx
@@ -12,13 +12,13 @@ import {
 import { Badge } from "../components/ToolBadges/ToolBadges";
 
 export const Technology: { [key: string]: Badge } = {
-  AWS: { name: "AWS", icon: SiAmazonaws },
-  Azure: { name: "Azure", icon: SiMicrosoftazure },
-  Docker: { name: "Docker", icon: SiDocker },
-  Express: { name: "Express", icon: SiExpress },
-  Jest: { name: "Jest", icon: SiJest },
-  MaterialUI: { name: "MaterialUI", icon: SiMaterialui },
-  React: { name: "React", icon: SiReact },
+  AWS: { name: "AWS", icon: SiAmazonaws, color: "#FF9900" },
+  Azure: { name: "Azure", icon: SiMicrosoftazure, color: "#0078D4" },
+  Docker: { name: "Docker", icon: SiDocker, color: "#2496ED" },
+  Express: { name: "Express.js", icon: SiExpress },
+  Jest: { name: "Jest", icon: SiJest, color: "#C21325" },
+  MaterialUI: { name: "Material UI", icon: SiMaterialui, color: "#007FFF" },
+  React: { name: "React", icon: SiReact, color: "#61DAFB" },
   SocketIO: { name: "Socket.io", icon: SiSocketdotio },
-  TypeScript: { name: "TypeScript", icon: SiTypescript },
+  TypeScript: { name: "TypeScript", icon: SiTypescript, color: "#3178C6" },
 };
