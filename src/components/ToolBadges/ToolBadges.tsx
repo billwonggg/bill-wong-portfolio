4e9c5aed1e058040b484ac9f9d9d723e50d5fc76
Bill Wong <billwong7788@gmail.com>
2022-09-27 00:08:36 +1000

    add icon props and jsx

--- src/components/ToolBadges/ToolBadges.tsx
+++ src/components/ToolBadges/ToolBadges.tsx
@@ -1,8 +1,12 @@
-import { Technology } from "../../models/Technology";
+import { IconType } from "react-icons";
 import styles from "./ToolBadges.module.scss";
 
+export interface Badge {
+  name: string;
+  icon: IconType;
+}
 interface ToolBadgesProps {
-  tools: Technology[];
+  tools: Badge[];
 }
 
 const ToolBadges = ({ tools }: ToolBadgesProps) => {
@@ -10,7 +14,8 @@ const ToolBadges = ({ tools }: ToolBadgesProps) => {
     <div className={styles.tools}>
       {tools.map((tool, i) => (
         <div className={styles.badge} key={i}>
-          {tool}
+          <tool.icon style={{ position: "relative", top: "2.5px" }} />
+          &nbsp;{tool.name}
         </div>
       ))}
     </div>
