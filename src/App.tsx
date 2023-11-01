7e4f2ec8a8cd88e7fca6e9ef98eaa1a455d626ae
Bill Wong <billwong7788@gmail.com>
2023-11-01 16:33:02 +1100

    Add certification page and route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ParticlesBg from "./components/ParticlesBg/ParticlesBg";
 import AboutPage from "./pages/About/AboutPage";
+import CertificationsPage from "./pages/Certifications/CertificationsPage";
 import ExpPage from "./pages/Experience/ExpPage";
 import HomePage from "./pages/Home/HomePage";
 import ProjectPage from "./pages/Projects/ProjectPage";
@@ -30,6 +31,7 @@ const App = () => {
           <Route path="/projects" element={<ProjectPage />} />
           <Route path="/experience" element={<ExpPage />} />
           <Route path="/skills" element={<SkillsPage />} />
+          <Route path="/certifications" element={<CertificationsPage />} />
         </Routes>
       </AnimatePresence>
     </BrowserRouter>
