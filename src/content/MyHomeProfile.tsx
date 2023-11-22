6e9dab02b13d6edc5bc96c4a841c63314d4e9a76
Bill Wong <billwong7788@gmail.com>
2023-11-22 18:18:04 +1100

    Update headlines

--- src/content/MyHomeProfile.tsx
+++ src/content/MyHomeProfile.tsx
@@ -3,8 +3,7 @@ import HomeProfile from "../models/HomeProfile";
 const MyHomeProfile: HomeProfile = {
   headlines: [
     "I am a third year Software Engineering student.",
-    "I am currently a Software Engineering intern at NAB.",
-    "I am an upcoming Software Engineering intern at Atlassian.",
+    "I am currently a Software Engineering intern at Atlassian.",
     "I have a great passion for technology.",
     "I am always eager to learn new skills.",
     "I enjoy working on full stack projects.",
