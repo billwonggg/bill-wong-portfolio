f83b0ab898151d4ad6e08833cd5a6ac86d46966e
Bill Wong <billwong7788@gmail.com>
2023-12-31 21:11:50 +1100

    Update config for new tsparticles version

--- src/components/ParticlesBg/ParticlesBg.tsx
+++ src/components/ParticlesBg/ParticlesBg.tsx
@@ -1,52 +1,56 @@
-import { useCallback } from "react";
-import Particles from "react-particles";
+import { type ISourceOptions } from "@tsparticles/engine";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { useEffect, useMemo, useState } from "react";
 import { loadFull } from "tsparticles";
-import type { Engine } from "tsparticles-engine";
 import { useWidthBreakpoint } from "../../util/breakpoint";
 
 const ParticlesBg = () => {
-  const customInit = useCallback(async (engine: Engine) => {
-    await loadFull(engine);
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
   }, []);
 
   const md = useWidthBreakpoint(900);
-
-  return (
-    <Particles
-      options={{
-        particles: {
-          move: {
-            direction: "right",
-            enable: true,
-            speed: md ? 0.5 : 1,
-            straight: true,
-          },
-          size: {
-            value: { min: 2, max: 4 },
-          },
-          opacity: {
-            animation: {
-              enable: true,
-              speed: 1,
-              sync: false,
-            },
-            value: { min: 0, max: 0.8 },
-          },
-          number: {
-            density: {
-              enable: true,
-              area: 400,
-            },
-            value: md ? 25 : 15,
-          },
-          zIndex: {
-            value: -10,
-          },
+  const options: ISourceOptions = useMemo(
+    () => ({
+      particles: {
+        move: {
+          direction: "right",
+          enable: true,
+          speed: md ? 0.5 : 1,
+          straight: true,
         },
-      }}
-      init={customInit}
-    />
+        size: {
+          value: { min: 2, max: 4 },
+        },
+        opacity: {
+          animation: {
+            enable: true,
+            speed: 1,
+            sync: false,
+          },
+          value: { min: 0, max: 0.8 },
+        },
+        number: {
+          density: {
+            enable: true,
+            area: 400,
+          },
+          value: md ? 25 : 15,
+        },
+        zIndex: {
+          value: -10,
+        },
+      },
+    }),
+    []
   );
+
+  return init ? <Particles options={options} id="tsparticles" /> : <></>;
 };
 
 export default ParticlesBg;
