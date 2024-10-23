25c162fabcc39a58060b0081ad06718bb96e6a0e
Bill Wong <billwong7788@gmail.com>
2024-10-23 23:43:45 +1100

    Use lazy loading for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import loadable from "@loadable/component";
 import { AnimatePresence } from "framer-motion";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ParticlesBg from "./components/ParticlesBg/ParticlesBg";
-import AboutPage from "./pages/About/AboutPage";
-import CertificationsPage from "./pages/Certifications/CertificationsPage";
-import ExpPage from "./pages/Experience/ExpPage";
-import HomePage from "./pages/Home/HomePage";
-import ProjectPage from "./pages/Projects/ProjectPage";
-import SkillsPage from "./pages/Skills/SkillsPage";
 import "./styles/global.scss";
 import { useAppHeight } from "./util/hooks";
+
+const AboutPage = loadable(() => import("./pages/About/AboutPage"));
+const CertificationsPage = loadable(() => import("./pages/Certifications/CertificationsPage"));
+const ExpPage = loadable(() => import("./pages/Experience/ExpPage"));
+const HomePage = loadable(() => import("./pages/Home/HomePage"));
+const ProjectPage = loadable(() => import("./pages/Projects/ProjectPage"));
+const SkillsPage = loadable(() => import("./pages/Skills/SkillsPage"));
 
 const App = () => {
   useAppHeight();
