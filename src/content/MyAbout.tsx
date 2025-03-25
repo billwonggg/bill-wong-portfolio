124b08c09a6f5726f1290762f9e973b0baf4617c
Bill Wong <billwong7788@gmail.com>
2025-03-25 23:52:12 +1100

    Brief intro update

--- src/content/MyAbout.tsx
+++ src/content/MyAbout.tsx
@@ -2,12 +2,10 @@ const MyAbout = () => {
   return (
     <div>
       <h1>About</h1>
-      <p>
-        I'm Bill, a 4th year Computer Science student at UNSW. I have been programming since high school and I love it!
-      </p>
+      <p>I'm Bill, a computer science graduate from UNSW. I have been programming since high school and I love it!</p>
       <p>
         I enjoy spending my spare time learning new technologies and developing new skills. I always try and apply the
-        knowledge I learned to real life projects, just like the one you are looking at right now! 😄
+        knowledge I learned to real life projects, just like the one you are looking at right now!
       </p>
       <br />
       <br />
